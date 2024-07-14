12233bb8ea05b6e68c99b657cc84ff92d522baa0
coc0nut <coc0nut@protonmail.com>
2024-07-14 15:38:27 +0200

    Update acore_setup.sh

--- acore_setup.sh
+++ acore_setup.sh
@@ -10,6 +10,39 @@ function ask_user() {
 
 sed -i "s|^TZ=.*$|TZ=$(cat /etc/timezone)|" src/.env
 
+sudo apt update
+
+# Check if MySQL client is installed
+if ! command -v mysql &> /dev/null
+then
+    echo "MySQL client is not installed. Installing mariadb-client now..."
+    sudo apt install -y mariadb-client
+else
+    echo "MySQL client is already installed."
+fi
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Installing Docker now..."
+    # Add Docker's official GPG key:
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+    echo "Docker is already installed."
+fi
+
+# Check if Azeroth Core is installed
 if [ -d "azerothcore-wotlk" ]; then
     cp src/.env azerothcore-wotlk/
     cp src/*.yml azerothcore-wotlk/
