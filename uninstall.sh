013ce91292a58ac27302aed49e52c9b913c6af22
coc0nut <coc0nut@protonmail.com>
2024-07-30 20:07:26 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-cd azerothcore-wotlk
+function ask_user() {
+    read -p "$1 (y/n): " choice
+    case "$choice" in
+        y|Y ) return 0;;
+        * ) return 1;;
+    esac
+}
 
-docker compose down
+if ask_user "This will uninstall azerothcore, continue?"; then
 
-docker system prune -a
+    cd azerothcore-wotlk
 
-docker volume rm azerothcore-wotlk_ac-client-data
-docker volume rm azerothcore-wotlk_ac-database
+    docker compose down
+    docker image prune -a
+    if ask_user "Delete volumes? If you keep the volumes, a reinstall will only update."; then
+        docker system prune -a
+        docker volume rm azerothcore-wotlk_ac-client-data
+        docker volume rm azerothcore-wotlk_ac-database
+    fi
+    
+    cd .. && rm -rf azerothcore-wotlk
 
-cd .. && rm -rf azerothcore-wotlk
-
-exit 0
+    exit 0
+fi
