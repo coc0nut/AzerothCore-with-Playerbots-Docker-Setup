3606629b654a3cfb3a9140042ad0fee8a4fcd576
coc0nut <coc0nut@protonmail.com>
2024-09-16 01:12:53 +0200

    16.09.24 01:12 - update

--- start_stop_acore.sh
+++ start_stop_acore.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 manage_docker_containers() {
-    # List of Docker containers
     containers=("ac-worldserver" "ac-authserver" "ac-database")
 
-    # Check if all containers are running
     all_running=true
     for container in "${containers[@]}"; do
         if ! docker ps --format '{{.Names}}' | grep -q "^${container}$"; then
@@ -14,15 +12,12 @@ manage_docker_containers() {
     done
 
     if $all_running; then
-        # Stop all containers if they are running
         echo "Stopping containers: ${containers[*]}"
         docker stop "${containers[@]}"
     else
-        # Start all containers if they are not running
         echo "Starting containers: ${containers[*]}"
         docker start "${containers[@]}"
     fi
 }
 
-# Call the function
 manage_docker_containers
